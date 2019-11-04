6234848acf668cda78d2e4c44b9a056e25d581ce
jvercimak <46680682+jvercimak@users.noreply.github.com>
2019-11-04 11:16:42 -0600

    Added showCard function  + typo fix
    prints an introduction to the insurance program to the user

--- eclipse-workspace/InsuranceApp/src/InsuranceScore.java
+++ eclipse-workspace/InsuranceApp/src/InsuranceScore.java
@@ -1,33 +1,46 @@
 import java.util.*;
 public class InsuranceScore {
-  		public static void showMenu() {
-		    System.out.println("\nHere are your choices: ");
-		    System.out.println("1. List members ");
-		    System.out.println("2. Add a new member");
-		    System.out.println("3. Save members");
-		    System.out.println("4. Load members");
-		    System.out.println("5. Assess members");
-		    System.out.println("6. Save assessments as JSON");
-		    System.out.println("7. Exit");
+	public static void showMenu() {
+		System.out.println("\nHere are your choices: ");
+		System.out.println("1. List members ");
+		System.out.println("2. Add a new member");
+		System.out.println("3. Save members");
+		System.out.println("4. Load members");
+		System.out.println("5. Assess members");
+		System.out.println("6. Save assessments as JSON");
+		System.out.println("7. Exit");
 		}
-	    public static void printSymbol(int howMany) {
-	        String symbol = "";
-	        for (int i = 0; i < howMany; i++) {
-	            symbol = symbol + "<>";
-	        }
+	public static void printSymbol(int howMany) {
+		String symbol = "";
+		for (int i = 0; i < howMany; i++) {
+			symbol = symbol + "<>";
+			}
 	        System.out.println(symbol);
 	    }
-		public static void showThanks() {
-				printSymbol(22);
-				System.out.println("             INSUREANCE SCORE CARD");
-				System.out.println("                  THANK YOU");
-				printSymbol(22);
-			}
+	public static void showCard() {
+		printSymbol(20);
+		System.out.println("	  INSURANCE SCORE CARD");
+		System.out.println("  This app scores a potential customer");
+		System.out.println("  on various health attributes: blood");
+		System.out.println("  pressure, age, height, weight, and");
+		System.out.println("  family history of disease. It writes");
+		System.out.println("  each member's insurance grade to a ");
+		System.out.println("  JSON file so that they can be easily");
+		System.out.println("  shared on a web-based data exchange.");
+		printSymbol(20);
+		}
+	public static void showThanks() {
+		printSymbol(22);
+		System.out.println("             INSURANCE SCORE CARD");
+		System.out.println("                  THANK YOU");
+		printSymbol(22);
+		}
 	public static void main(String []args) {
 		int choice;
 		Scanner sc = new Scanner(System.in);
-        do {
-            showMenu();
+		showCard();
+		do {
+			showMenu();
             System.out.println("Enter your choice: ");
             choice = sc.nextInt();
             if (choice == 1) {
@@ -46,5 +59,4 @@ public class InsuranceScore {
             } while (choice != 7);
         showThanks();
         }
-	}
 }
